f70b8ffdfedf1e74e23ad2ccc37cbeef3a7a75a6
Agustin586 <marcelo123z.com@gmail.com>
2022-04-05 23:18:43 -0300

    --

--- P3_E18/P3_E18.c
+++ P3_E18/P3_E18.c
@@ -83,3 +83,114 @@
 
     Fin.
 */
+
+/*
+
+Metodologia:
+    - Ingresar las notas por teclado las 7 notas
+    - Se debe calcular y mostrar el promedio de ese alumno y guardarlo en un promedio general
+    - Se debe preguntar si se quiere seguir ingresando datos
+
+Algoritmo:
+
+    Variables:
+        Entero:     i,ContAlum
+        Real:       PromAlum,PromGnerl,SumAlm,Nota
+        Caracter:   Valid
+
+    Inicio:
+        ContAlm <- 0
+        PromGnerl <- 0
+        Valid <- 'y'
+
+        Repetir Mientras ( Valid = 'y' )
+            SumAlm <- 0
+            PromAlm <- 0
+            
+            Repetir Para i <- 1,7,1
+                Escribir ( "Nota ",i,",alumno ",ContAlm+1,":" )
+                Leer (Nota)
+                Repetir Mientras ( Nota <= 0  OR Nota > 10 )
+                    Escribir ( "Ingrese la nota de nuevo:" )
+                    Leer ( Nota )
+                Fin mientras
+                SumAlm <- SumAlm + Nota
+            Fin para
+
+            ContAlm <- ContAlm + 1
+
+            PromAlm <- SumAlm / 7.0
+            Escribir ( "Promedio de alumno ",ContAlm,":",PromAlm )
+
+            PromGneral <- PromGnerl + PromAlm
+
+            Escribir ("Desea ingresar mas datos:Si(y),No(n)")
+            Leer (Valid)
+            
+            Repetir Mientras ( Valid <> 'y' AND Valid <>'n' )
+                Escribir ("Vuelva a ingresar la opcion:Si(y),No(n)")
+                Leer (valid)
+            Fin mientras
+
+        Fin mientras
+
+        PromGnerl <- PromGnerl / ContAlm
+        Escribir ("El promedio general del curso es:",PromGnerl)
+    Fin.
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+#define CANT_NOTAS 7
+#define NOTA_MAX 10
+#define NOTA_MIN 1
+
+int main ()
+{
+    int ContAlm;
+    float PromAlm,PromGnr,Nota,SumaAlm;
+    char Valid;
+
+    //Inicializacion de datos
+    ContAlm = 0;
+    PromAlm = 0;
+    SumaAlm = 0;
+    PromGnr = 0;
+    Valid = 'y';
+
+    //Bucle
+    while ( Valid == 'y' || Valid == 'Y' )
+    {
+        SumaAlm = 0;
+        PromAlm = 0;
+        for ( int i = 0; i < CANT_NOTAS; i++)
+        {
+            printf ( "Ingrese la nota %d del alumno %d:",i+1,ContAlm+1 );
+            scanf ( "%f",&Nota);
+            while ( Nota < NOTA_MIN || Nota > NOTA_MAX )
+            {
+                printf ( "Ingrese una nota valida\n" );
+                printf ( "Nota %d,alumno %d:",i+1,ContAlm+1 );
+                scanf ( "%f",&Nota );
+            }
+            SumaAlm = SumaAlm + Nota;
+        }
+        PromAlm = SumaAlm / CANT_NOTAS;
+        PromGnr = PromGnr + PromAlm;
+        ContAlm += 1;
+        printf ( "El promedio general del alumno %d es:%.2f\n",ContAlm,PromAlm);
+        printf ( "Desea seguir ingresando datos.Si(y),No(n):" );
+        scanf ( " %c",&Valid );
+        while ( Valid != 'y' && Valid != 'n' )
+        {
+            printf ( "Ingrese una opcion valida\n" );
+            printf ( "Si(y),No(n):" );
+            scanf ( " %c",&Valid );
+        }
+    }
+    printf ( "El promedio general de los alumnos es %.2f",(PromGnr=PromGnr/ContAlm) );
+
+    getch();
+    return 0;    
+}
--- P3_E19/P3_E19.c
+++ P3_E19/P3_E19.c
@@ -35,3 +35,101 @@
         - Calcula e imprime el promedio maximo y la comision que lo tuvo
 
 */
+
+/*
+
+Metodologia:
+  - Ingresan el valor de la edad de los estudiantes de esa comision
+  - Calcular el promedio de la edad de los estudiantes
+  - Bucle de comision
+    - Bucle de los estudiantes
+      - Ingresa los datos del estudiante
+  - Determina si se superó la promedio de edad
+
+Algoritmo:
+    Variables:
+        Entera: Edad,Comi,SumComi,ContAlm,ComiMayor
+        Real: PromComi,Aux
+    Inicio:
+        // Inicializacion
+        PromComi <- 0 
+        SumaComi <- 0
+        ContAlm <- 1
+        ComiMayor <- 0
+
+        // Algoritmo
+        Escribir ( "Comision:" )
+        Leer ( Comi )
+        Repetir Mientras ( Comi <> 0 )
+            SumAlm <- 0
+            ContAlm <- 1
+            Escribir ( "Comision ",Comi," Almuno:",ContAlm )
+            Leer ( Edad )
+            Repetir Mientras ( Edad >= 0  AND Edad <= 100 )
+                SumaComi <- SumaComi + Edad
+                ContAlm <- ContAlm + 1
+                Escribir ( "Comision ",Comi," Almuno:",ContAlm )
+                Leer ( Edad )
+                Si ( Edad = 0 ) entonces:
+                    ContAlm <- ContAlm - 1
+                Fin si
+            Fin mientras
+            Aux <- SumaComi / ContAlm
+            Si ( PromComi < Aux ) entonces:
+                PromComi <- Aux
+                ComiMayor <- Comi
+            Fin si
+            Escribir ( "Nueva Comision:" )
+            Leer ( Comi )
+        Fin mientras
+        Escribir ( "Comision con mayor promedio:",ComiMayor )
+    Fin.
+ */
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdint.h>
+
+int main ()
+{
+    uint8_t Edad,SumaComi,ContAlm,ComiMayor;
+    float PromComi,Aux;
+    int Comi;
+
+    // Inicializacion
+    PromComi = 0;
+    SumaComi = 0;
+    ContAlm = 1;
+    ComiMayor = 0;
+
+    printf ( "Nueva Comision:" );
+    scanf ( "%d",&Comi );
+
+    while ( Comi != 0 )
+    {
+        SumaComi = 0;
+        ContAlm =  1;
+        printf ( "Comision:%d\nAlumno %d:",Comi,ContAlm );
+        scanf ( "%d",&Edad );
+        while ( Edad > 0 && Edad < 100 )
+        {
+            ContAlm += 1;
+            SumaComi = SumaComi + Edad;
+            printf ( "Comision:%d\nAlmumno %d:",Comi,ContAlm );
+            scanf  ( "%d",&Edad );
+            if ( Edad == 0 )    ContAlm --;
+        }
+        Aux = SumaComi / ContAlm;
+        if ( PromComi < Aux )
+        {
+            PromComi = Aux;
+            ComiMayor = Comi;
+        }
+        printf ( "Nueva Comision:" );
+        scanf ( "%d",&Comi );
+    }
+    printf ( "Comision de mayor promedio:%d",ComiMayor );
+
+    getch();
+    return 0;
+}
